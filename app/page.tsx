b3257339a368514e1488b5131c4f21b26be5a32d
DipalokSen <aksharsen68@gmail.com>
2025-06-05 20:42:28 +0600

    Implement HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,72 @@
+import CompanionCard from '@/Components/CompanionCard'
+import CompanionList from '@/Components/CompanionList'
+import Cta from '@/Components/Cta'
 import React from 'react'
+import { recentSessions } from '@/constants'
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <>
+    
+   
+    <main>
+  
+      <h1>Dashboard</h1>
+
+      <section className='home-section'>
+          <CompanionCard
+          
+          id="556"
+          name="neura the brainy exploler"
+          topic="neural network of the brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+
+          
+          
+          
+          />
+          <CompanionCard
+          id="557"
+          name="neura the brainy exploler"
+          topic="neural network of the brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+
+          
+          
+          
+          />
+          <CompanionCard
+          id="558"
+          name="neura the brainy exploler"
+          topic="neural network of the brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+
+          
+          
+          
+          />
+
+      </section>
+
+      <section className='home-section'>
+         <CompanionList 
+        title="Recently completed sessions"
+        companions={recentSessions}
+        classNames="w-2/3 max-lg:w-full"
+         />
+      </section>
+
+      <section className='home-section'>
+       <Cta/>
+      </section>
+    </main>
+     </>
   )
 }
 
